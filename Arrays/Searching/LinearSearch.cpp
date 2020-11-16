d01b5f947ad0eac494b74dc57e326cd56e14b526
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-11-16 13:28:30 +0530

    Update LinearSearch.cpp

--- Arrays/Searching/LinearSearch.cpp
+++ Arrays/Searching/LinearSearch.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+
+using namespace std;
+
+int linearSearch(int arr[], int n, int key){
+    for(int i=0; i<n; i++){
+        if(arr[i]==key){
+            return i;
+        }
+
+    }
+    return -1;
+}
+
+int main()
+{//Searching in arrays
+
+int key;
+int n;
+cin>>n;
+
+int arr[n];
+
+for(int i=0; i<n; i++){
+    cin>>arr[i];
+}
+
+cin>>key;
+
+cout<<linearSearch(arr,n,key)<<endl;
+
+return 0;
+}
