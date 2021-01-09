b34ed215a8ffdf7b403e86b3cd6e49a1d299927c
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2021-01-09 21:08:39 +0530

    Update BinarySearch.cpp

--- Arrays/Searching/BinarySearch.cpp
+++ Arrays/Searching/BinarySearch.cpp
@@ -2,36 +2,43 @@
 
 using namespace std;
 
-int binarySearch(int arr[], int n,int key){
+int binsearch(int arr[], int n, int key){
 int s=0;
 int e=n;
-while(s<=e){
-    innt mid =(s+e)/2;
-    if (arr[mid]==key){
-        return mid;
+
+
+while(s<e){
+    int mid=(s+e)/2;
+    if(arr[mid]<key){
+        s=mid+1;
     }
-    else if (arr[mid]>key){
+    else if(arr[mid]>key){
         e=mid-1;
-        
     }
     else{
-        s=mid+1;
-        
+        return mid;
+    }    
+}
+
+}
+
+int main(){
+int n, i;
+    cout<<"Enter the value of n";
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n;i++){
+        cin>>arr[i];
     }
-}
-return -1;
-}
 
-int main()
-{ int n, i;
-cin>>n;
-int arr[n];
-for(int i=0; i<n; i++){
-    cin>>arr[i];
-}
+    for(i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+
+    }
 int key;
+cout<<"Enter the key:";
 cin>>key;
-cout<<binarySearch(arr, n, key);
+cout<<binsearch(arr, n, key)<<endl;
 
-    return 0;
+return 0;
 }
